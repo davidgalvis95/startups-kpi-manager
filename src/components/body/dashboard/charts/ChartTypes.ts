719083c7d0f7cdf7592756f73fa2613079039d6d
David Galvis <david.galvis@ymedialabs.com>
2022-03-09 03:16:42 -0500

    Updating chart types

--- src/components/body/dashboard/charts/ChartTypes.ts
+++ src/components/body/dashboard/charts/ChartTypes.ts
@@ -1,8 +1,8 @@
 enum ChartTypes {
-    LINE="line", 
-    BAR="bar", 
-    STACKED_BAR="stackedBar", 
-    RING="ring"
+    LINE="Linea", 
+    BAR="Barras", 
+    STACKED_BAR="Barras Agrupadas", 
+    RING="Anillo"
 }
 
 export default ChartTypes;
